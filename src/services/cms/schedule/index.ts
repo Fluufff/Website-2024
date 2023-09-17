fdb501b78f720c77adde27fa772e1f66e791cd6e
Bleats <bleats@fluufff.org>
2023-09-17 21:39:16 +0200

    feat: add cache tags on cms fetch request

--- src/services/cms/schedule/index.ts
+++ src/services/cms/schedule/index.ts
@@ -5,6 +5,8 @@ export type { Schedule, ScheduleEvent, ScheduleLocation } from './data';
 
 export async function getSchedule(lang: string): Promise<Schedule> {
   // TODO: cache invalidation
-  const res = await fetchCmsSiteData('content');
+  const res = await fetchCmsSiteData('content', {
+    next: { tags: ['cms', 'cms.schedule'] },
+  });
   return parseSchedule(await res.json());
 }
