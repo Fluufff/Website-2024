2088b0982e499b9c765677f710401e10e3bec7e9
Bleats <bleats@fluufff.org>
2023-10-01 22:11:25 +0200

    feat: use cms content type endpoint

--- src/services/cms/schedule/index.ts
+++ src/services/cms/schedule/index.ts
@@ -2,10 +2,18 @@ import { fetchCmsSiteData } from '../util';
 
 import { parseSchedule, Schedule } from './data';
 
+// TODO: move and assert
+const contentTypes = process.env.CMS_CONTENT_TYPE_EVENT!;
+
 export type { Schedule, ScheduleEvent, ScheduleLocation } from './data';
 
 export async function getSchedule(lang: string): Promise<Schedule> {
-  const queryString = new URLSearchParams({ lang, pagesize: '-1' });
+  const queryString = new URLSearchParams({
+    lang,
+    pagesize: '-1',
+    contentTypes,
+    populate: 'true',
+  });
 
   // TODO: cache invalidation
   const res = await fetchCmsSiteData('content?' + queryString, {
