2df206e8beae11dff10ac65799298a85b3573910
Bleats <bleats@fluufff.org>
2024-01-01 20:47:35 +0100

    fix: configure messages once

--- src/helpers/localization.ts
+++ src/helpers/localization.ts
@@ -1,19 +1,8 @@
-import { cache } from 'react';
-
 import { localeKeys, SupportedLocale } from '@/config';
 
 export type PropsWithLocale<P = {}> = P & { params: { locale: string } };
 
 export type Messages = typeof import('@/messages/en.json');
-
-export const getMessages = cache(
-  async (locale: SupportedLocale): Promise<Messages> => {
-    // The import is probably already cached by the module system, but too
-    // low-level to my taste. Also, this is where we can implement a conversion
-    // from other formats into json.
-    return (await import(`@/messages/${locale}.json`)).default;
-  },
-);
 
 export function isSupportedLocale(x: string): x is SupportedLocale {
   return (localeKeys as readonly string[]).includes(x);
