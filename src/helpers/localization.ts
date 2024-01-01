f993741d66cbc1cb001d6d5df526c440f3be8288
Bleats <bleats@fluufff.org>
2024-01-01 20:48:40 +0100

    feat: typed next-intl messages

--- src/helpers/localization.ts
+++ src/helpers/localization.ts
@@ -2,8 +2,6 @@ import { localeKeys, SupportedLocale } from '@/config';
 
 export type PropsWithLocale<P = {}> = P & { params: { locale: string } };
 
-export type Messages = typeof import('@/messages/en.json');
-
 export function isSupportedLocale(x: string): x is SupportedLocale {
   return (localeKeys as readonly string[]).includes(x);
 }
