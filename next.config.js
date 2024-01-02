53f76e1556c54e6a9920d411a758510822cd3392
Bleats <bleats@fluufff.org>
2024-01-02 22:18:35 +0100

    feat: setup mdx components
    This follows docs, except for the part about setting up mdx as an
    extension for `page.mdx`:
    
    https://nextjs.org/docs/app/building-your-application/configuring/mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
+const withMDX = require('@next/mdx')();
 const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-module.exports = withNextIntl(nextConfig);
+module.exports = withMDX(withNextIntl(nextConfig));
---
+++ src/mdx-components.tsx
@@ -0,0 +1,9 @@
+// https://nextjs.org/docs/app/building-your-application/configuring/mdx
+
+import type { MDXComponents } from 'mdx/types';
+
+export function useMDXComponents(components: MDXComponents): MDXComponents {
+  return {
+    ...components,
+  };
+}
