e71c79922ed3a9e666b14ec995df5df2f86d6002
Bleats <bleats@fluufff.org>
2024-02-17 22:25:46 +0100

    feat: validate environment at build time
    This is mostly useful with `next dev`, as `next build` was already
    raising errors at build time.
    
    This follows recommendations from T3 Env:
    https://env.t3.gg/docs/introduction

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+/* eslint-disable import/order */
 const withMDX = require('@next/mdx')();
 const withNextIntl = require('next-intl/plugin')();
+
+const jiti = require('jiti')(__filename);
+
+// important: we load this so that validation errors are raised at build time
+const { env } = jiti('./src/env.ts');
 
 /** Webpack rule alllowing to bundle a file as-is by importing it with suffix
  * `?raw`.
@@ -18,7 +24,7 @@ const rawAssetWebpackRule = {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: process.env.STANDALONE_OUTPUT ? 'standalone' : undefined,
+  output: env.STANDALONE_OUTPUT ? 'standalone' : undefined,
 
   webpack: (config) => {
     config.module.rules.push(rawAssetWebpackRule);
