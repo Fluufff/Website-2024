9e7a1939d5ae3b11c155d54fb2033654c2ff97db
Bleats <bleats@fluufff.org>
2024-05-10 21:52:44 +0200

    feat: cache draco loader

--- src/app/[locale]/_scene/constants.ts
+++ src/app/[locale]/_scene/constants.ts
@@ -1,4 +1,10 @@
+import { env } from '@/env';
+
 // The DRACOLoader from three.js generates its own URLs at runtime. This forces
 // us to put the files in the `public` assets directory instead of bundling them
-// (with content hashes) in webpack. See next.config.js.
-export const DRACO_FOLDER = '/draco/';
+// (with content hashes) in webpack using a raw asset rule (see next.config.js).
+//
+// We therefore also need to point to the CDN ourselves. If this stops working
+// the 3D scene on the home page will not work, so the likelihood of silent
+// breakage is low.
+export const DRACO_FOLDER = `${env.NEXT_PUBLIC_ASSET_PREFIX ?? ''}/draco/`;
