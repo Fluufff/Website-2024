ff6997993122c2a568e2e21dc196140e53f72ce9
Bleats <bleats@fluufff.org>
2024-11-03 21:43:11 +0100

    feat: ics export accessible on /schedule.ics (English only)

---
+++ src/services/cms/schedule/exportIcal.ts
@@ -0,0 +1,45 @@
+import ical, { ICalEventData } from 'ical-generator';
+import sanitize from 'sanitize-html';
+
+import { Schedule, ScheduleEvent, ScheduleLocation } from './data';
+
+/**
+ * Turn a Schedule into ical file contents.
+ *
+ * Does not bother with time zones, uses UTC.
+ */
+export function toIcal(schedule: Schedule): string {
+  const cal = ical({ name: 'Flüüfff events' });
+
+  for (const event of schedule.events) {
+    cal.createEvent(
+      toEventData(event, schedule.locationById[event.locationId]),
+    );
+  }
+
+  return cal.toString();
+}
+
+function toEventData(
+  event: ScheduleEvent,
+  location: ScheduleLocation,
+): ICalEventData {
+  const description = [
+    event.hostName && `Presented by ${event.hostName}`,
+    noHtml(event.htmlDescription),
+  ]
+    .filter(Boolean)
+    .join('\n\n');
+
+  return {
+    summary: event.name,
+    location: location.name,
+    start: event.startTime,
+    end: event.endTime,
+    description,
+  };
+}
+
+function noHtml(htmlString: string): string {
+  return sanitize(htmlString, { allowedTags: [] });
+}
